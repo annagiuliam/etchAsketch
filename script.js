e93db36f970dd1decf5974e1ecd76e0752218d17
Anna <annagiulia.musso@gmail.com>
2020-08-21 04:08:28 -0500

    work inprogress

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 // set constants and variables
-const container = document.querySelector("#container");
-// let rows = document.getElementsByClassName("gridRow");
-// let cells = document.getElementsByClassName("cell");
-
-
-// function makeRows(rowNum) {
-
-//     for (r = 0; r < rowNum; r++) {
-//         let row = document.createElement("div");
-//         row.classList.add("row");
-//         container.appendChild(row);
-//     };
-// };
+const sketchContainer = document.querySelector("#sketchContainer");
+const btnClear = document.querySelector("#clear");
 
 
 
-function createRow(cellNum) {
-    for (let i = 0; i < cellNum; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        container.appendChild(cell);
-    }
+function createGrid() {
+    let size = Number(getComputedStyle(document.documentElement).getPropertyValue("--grid-size"));
+    sketchContainer.style.setProperty("--grid-size", size);
+    for (let i = 0; i < size * size; i++) {
+        
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            sketchContainer.appendChild(cell);
+                    
+    }   
 }
 
-createRow(5);
+function hoverEffect() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {    
+    cell.addEventListener("mouseover", () => { 
+        cell.classList.add("hover"); // OR event.target.style.backgroundColor = "red";
+     });
+    });
+}
+
+btnClear.addEventListener("click", () => {    
+    const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {            
+                cell.classList.remove("hover"); // toggle will turn whitw these and turn blue the ones wihtout hover
+        });
+    let size = prompt("Enter a new size for the sketch pad");
+    document.documentElement.style.setProperty('--grid-size', size);
+    etchAsketch();
+    });
+
+
+
+function etchAsketch() {
+    createGrid();
+    hoverEffect();
+}
+
+
+etchAsketch();
